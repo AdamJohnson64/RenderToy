dc9f7b9843958b8e99cc22cba55003b5cafeafac
Adam Johnson <adamcplusplus.johnson@gmail.com>
2017-01-02 15:57:37 -0800

    Put the geometrics in order.

--- RenderToyWPF/Geometry/Geometry.cs
+++ RenderToyWPF/Geometry/Geometry.cs
@@ -7,6 +7,44 @@ namespace RenderToy
     {
         Point3D GetPointUV(double u, double v);
     }
+    public class BezierPatch : IParametricUV
+    {
+        public BezierPatch()
+        {
+            // Define the hull for the patch.
+            const double h = 0.5;
+            hull = new Point3D[16]
+            {
+                new Point3D(-1, 0, -1), new Point3D(-h, 0, -1), new Point3D(+h, 0, -1), new Point3D(+1, 0, -1),
+                new Point3D(-1, 0, -h), new Point3D(-h, 0, -h), new Point3D(+h, 0, -h), new Point3D(+1, 0, -h),
+                new Point3D(-1, 0, +h), new Point3D(-h, 0, +h), new Point3D(+h, 0, +h), new Point3D(+1, 0, +h),
+                new Point3D(-1, 0, +1), new Point3D(-h, 0, +1), new Point3D(+h, 0, +1), new Point3D(+1, 0, +1),
+            };
+        }
+        public BezierPatch(Point3D[] hull)
+        {
+            this.hull = hull;
+        }
+        public Point3D GetPointUV(double u, double v)
+        {
+            // The Bernstein polynomial factors.
+            double nu = 1 - u;
+            double[] bu = new double[4] { nu * nu * nu, 3 * u * nu * nu, 3 * u * u * nu, u * u * u };
+            double nv = 1 - v;
+            double[] bv = new double[4] { nv * nv * nv, 3 * v * nv * nv, 3 * v * v * nv, v * v * v };
+            // Compute the UV point.
+            Point3D acc = new Point3D(0, 0, 0);
+            for (int j = 0; j < 4; ++j)
+            {
+                for (int i = 0; i < 4; ++i)
+                {
+                    acc = MathHelp.Add(acc, MathHelp.Scale(hull[i + j * 4], bu[i] * bv[j]));
+                }
+            }
+            return acc;
+        }
+        Point3D[] hull = null;
+    }
     public class Cylinder : IParametricUV
     {
         public Point3D GetPointUV(double u, double v)
@@ -31,36 +69,6 @@ namespace RenderToy
             double vcos = Math.Cos(v * Math.PI);
             double vsin = Math.Sin(v * Math.PI);
             return new Point3D(-usin * vsin, vcos, ucos * vsin);
-        }
-    }
-    public class BezierPatch : IParametricUV
-    {
-        public Point3D GetPointUV(double u, double v)
-        {
-            // Define the hull for the patch.
-            const double h = 0.5;
-            Point3D[] hull = new Point3D[16]
-            {
-                new Point3D(-1, 0, -1), new Point3D(-h, 0, -1), new Point3D(+h, 0, -1), new Point3D(+1, 0, -1),
-                new Point3D(-1, 0, -h), new Point3D(-h, 1, -h), new Point3D(+h, 0, -h), new Point3D(+1, 0, -h),
-                new Point3D(-1, 0, +h), new Point3D(-h, 0, +h), new Point3D(+h, 0, +h), new Point3D(+1, 0, +h),
-                new Point3D(-1, 0, +1), new Point3D(-h, 0, +1), new Point3D(+h, 0, +1), new Point3D(+1, 0, +1),
-            };
-            // The Bernstein polynomial factors.
-            double nu = 1 - u;
-            double[] bu = new double[4] { nu * nu * nu, 3 * u * nu * nu, 3 * u * u * nu, u * u * u };
-            double nv = 1 - v;
-            double[] bv = new double[4] { nv * nv * nv, 3 * v * nv * nv, 3 * v * v * nv, v * v * v };
-            // Compute the UV point.
-            Point3D acc = new Point3D(0, 0, 0);
-            for (int j = 0; j < 4; ++j)
-            {
-                for (int i = 0; i < 4; ++i)
-                {
-                    acc = MathHelp.Add(acc, MathHelp.Scale(hull[i + j * 4], bu[i] * bv[j]));
-                }
-            }
-            return acc;
         }
     }
 }
