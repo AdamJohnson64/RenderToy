21f31df7fe3fe95d63e30cdf0210c8bfcfa29aca
Adam Johnson <adamcplusplus.johnson@gmail.com>
2017-01-10 19:24:46 -0800

    Prepare for reflections.

--- RenderToyWPF/Render/Raytrace.cs
+++ RenderToyWPF/Render/Raytrace.cs
@@ -20,9 +20,8 @@ namespace RenderToy
                 .Where(x => x.Node.Primitive is IRayTest)
                 .Select(x => new RaytraceObject(x.Transform, (IRayTest)x.Node.Primitive, ColorToARGB(x.Node.WireColor), x.Node.material))
                 .ToArray();
-            Vector3D light_vector = new Vector3D(1, 1, -1);
-            light_vector.Normalize();
             // Render the pixel buffer for the raytrace result.
+            Point3D found_color = new Point3D(0, 0, 0);
             for (int y = 0; y < buffer_height; ++y)
             {
                 for (int x = 0; x < buffer_width; ++x)
@@ -32,86 +31,96 @@ namespace RenderToy
                     Point4D v42 = inverse_mvp.Transform(new Point4D(-1.0 + ((x * 2.0) + 0.5) / buffer_width, 1.0 - ((y * 2.0) + 0.5) / buffer_height, 1, 1));
                     Point3D ray_origin = new Point3D(v41.X / v41.W, v41.Y / v41.W, v41.Z / v41.W);
                     Vector3D ray_direction = new Point3D(v42.X / v42.W, v42.Y / v42.W, v42.Z / v42.W) - ray_origin;
-                    // Intersect test this ray against everything in the scene.
-                    RaytraceObject found_object = null;
-                    double found_lambda = double.PositiveInfinity;
-                    foreach (var test in objects)
+                    if (!RayColor(objects, ray_origin, ray_direction, ref found_color)) continue;
+                    uint color = Point3ToARGB(found_color);
+                    unsafe
                     {
-                        double lambda = test.RayTestDistance(ray_origin, ray_direction);
-                        if (lambda >= 0 && lambda < found_lambda)
-                        {
-                            found_object = test;
-                            found_lambda = lambda;
-                        }
-                    }
-                    // If we hit something then output the pixel.
-                    if (found_lambda != double.PositiveInfinity)
-                    {
-                        // World Vector - The world space position of intersection.
-                        Point3D vector_world = ray_origin + found_lambda * ray_direction;
-                        // Normal Vector - The world space normal of the intersection point.
-                        Vector3D vector_normal = found_object.RayTestNormal(ray_origin, ray_direction);
-                        vector_normal.Normalize();
-                        // View Vector - The world space direction we are looking.
-                        Vector3D vector_view = -ray_direction;
-                        vector_view.Normalize();
-                        // Light Vector - The world space direction toward the light.
-                        Vector3D vector_light = new Point3D(10, 10, -10) - vector_world;
-                        vector_light.Normalize();
-                        // Reflection Vector - The world space reflected view about the normal.
-                        Vector3D vector_reflect = -vector_view + 2 * MathHelp.Dot(vector_normal, vector_view) * vector_normal;
-                        // Shadow test.
-                        Point3D shadow_origin = ray_origin + found_lambda * ray_direction + 0.0001 * vector_normal;
-                        double shadow_multiplier = RayShadowTest(objects, shadow_origin, light_vector) ? 0.5 : 1.0;
-                        //double shadow_multiplier = found_shadow_lambda == double.PositiveInfinity ? 1 : 0.5;
-                        // Calculate material color.
-                        Color color_material = found_object.material.MaterialCompute(ray_origin, ray_direction, found_lambda);
-                        // Lighting scales.
-                        double lambert = Math.Max(0, Math.Min(1, MathHelp.Dot(vector_normal, vector_light) * shadow_multiplier));
-                        double specular = Math.Pow(Math.Max(0, Math.Min(1, MathHelp.Dot(vector_reflect, vector_light))), 10);
-                        // Compute the overall color of the surface.
-                        double r = lambert * color_material.R / 255.0 + specular;
-                        double g = lambert * color_material.G / 255.0 + specular;
-                        double b = lambert * color_material.B / 255.0 + specular;
-                        // Clamp the color components so we don't oversaturate.
-                        r = Math.Max(0, Math.Min(1, r));
-                        g = Math.Max(0, Math.Min(1, g));
-                        b = Math.Max(0, Math.Min(1, b));
-                        // Compute the final pixel.
-                        uint color =
-                            (0xffU << 24) |             // Alpha (MSB)
-                            ((uint)(r * 0xffU) << 16) | // Red
-                            ((uint)(g * 0xffU) << 8) |  // Green
-                            ((uint)(b * 0xffU) << 0);   // Blue (LSB)
-                        unsafe
-                        {
-                            byte* pRaster = (byte*)buffer_memory + y * buffer_stride;
-                            byte* pPixel = pRaster + x * 4;
-                            *(uint*)pPixel = color;
-                        }
+                        byte* pRaster = (byte*)buffer_memory + y * buffer_stride;
+                        byte* pPixel = pRaster + x * 4;
+                        *(uint*)pPixel = color;
                     }
                 }
             }
         }
-        static bool RayShadowTest(RaytraceObject[] objects, Point3D shadow_origin, Vector3D light_vector)
+        static bool RayColor(RaytraceObject[] objects, Point3D ray_origin, Vector3D ray_direction, ref Point3D output_color)
+        {
+            // Intersect test this ray against everything in the scene.
+            double found_lambda = double.PositiveInfinity;
+            RaytraceObject found_object = null;
+            RayIntersectFirst(objects, ray_origin, ray_direction, ref found_lambda, ref found_object);
+            // If we hit something then output the pixel.
+            if (double.IsPositiveInfinity(found_lambda)) return false;
+            // World Vector - The world space position of intersection.
+            Point3D vector_world = ray_origin + found_lambda * ray_direction;
+            // Normal Vector - The world space normal of the intersection point.
+            Vector3D vector_normal = found_object.RayTestNormal(ray_origin, ray_direction);
+            vector_normal.Normalize();
+            // View Vector - The world space direction we are looking.
+            Vector3D vector_view = -ray_direction;
+            vector_view.Normalize();
+            // Light Vector - The world space direction toward the light.
+            Vector3D vector_light = new Point3D(10, 10, -10) - vector_world;
+            vector_light.Normalize();
+            // Reflection Vector - The world space reflected view about the normal.
+            Vector3D vector_reflect = -vector_view + 2 * MathHelp.Dot(vector_normal, vector_view) * vector_normal;
+            // Shadow test.
+            Point3D shadow_origin = ray_origin + found_lambda * ray_direction + 0.0001 * vector_normal;
+            double shadow_multiplier = RayIntersectAny(objects, shadow_origin, vector_light) ? 0.5 : 1.0;
+            // Calculate material color.
+            Color color_material = found_object.material.MaterialCompute(ray_origin, ray_direction, found_lambda);
+            // Lighting scales.
+            double lambert = Math.Max(0, Math.Min(1, MathHelp.Dot(vector_normal, vector_light) * shadow_multiplier));
+            double specular = Math.Pow(Math.Max(0, Math.Min(1, MathHelp.Dot(vector_reflect, vector_light))), 10);
+            // Compute the overall color of the surface.
+            output_color.X = lambert * color_material.R / 255.0 + specular;
+            output_color.Y = lambert * color_material.G / 255.0 + specular;
+            output_color.Z = lambert * color_material.B / 255.0 + specular;
+            return true;
+        }
+        static bool RayIntersectAny(RaytraceObject[] objects, Point3D ray_origin, Vector3D ray_direction)
         {
             foreach (var test in objects)
             {
-                double lambda = test.RayTestDistance(shadow_origin, light_vector);
-                if (lambda >= 0 && !double.IsPositiveInfinity(lambda))
-                {
-                    return true;
-                }
+                double lambda = test.RayTestDistance(ray_origin, ray_direction);
+                if (lambda >= 0 && !double.IsPositiveInfinity(lambda)) return true;
             }
             return false;
         }
-        private static uint ColorToARGB(Color color)
+        static void RayIntersectFirst(RaytraceObject[] objects, Point3D ray_origin, Vector3D ray_direction, ref double output_lambda, ref RaytraceObject output_object)
+        {
+            double found_lambda = double.PositiveInfinity;
+            RaytraceObject found_object = null;
+            foreach (var test in objects)
+            {
+                double lambda = test.RayTestDistance(ray_origin, ray_direction);
+                if (lambda >= 0 && lambda < found_lambda)
+                {
+                    found_object = test;
+                    found_lambda = lambda;
+                }
+            }
+            output_lambda = found_lambda;
+            output_object = found_object;
+        }
+        static uint ColorToARGB(Color color)
         {
             return
                 ((uint)color.A << 24) |
                 ((uint)color.R << 16) |
                 ((uint)color.G << 8) |
                 ((uint)color.B << 0);
+        }
+        static uint Point3ToARGB(Point3D color)
+        {
+            // Clamp the color components so we don't oversaturate.
+            double r = Math.Max(0, Math.Min(1, color.X));
+            double g = Math.Max(0, Math.Min(1, color.Y));
+            double b = Math.Max(0, Math.Min(1, color.Z));
+            return
+                (0xffU << 24) |             // Alpha (MSB)
+                ((uint)(r * 0xffU) << 16) | // Red
+                ((uint)(g * 0xffU) << 8) |  // Green
+                ((uint)(b * 0xffU) << 0);   // Blue (LSB)
         }
     }
     /// <summary>
