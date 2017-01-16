93b678a1aa2e4848284df47e039a59d37ff9ca2a
Adam Johnson <adamcplusplus.johnson@gmail.com>
2017-01-15 16:03:42 -0800

    Switch the scene to data driven.
    Also add in Schlick approximation for reflectance.

--- RenderToyD3D9/RenderToyCUDA.cu
+++ RenderToyD3D9/RenderToyCUDA.cu
@@ -20,6 +20,7 @@ __device__ double3 operator/(const double3 &lhs, double rhs) { return lhs * (1 /
 __device__ double4 operator/(const double4 &lhs, double rhs) { return lhs * (1 / rhs); }
 
 // Common Math Primitives.
+__device__ double Clamp(double min, double max, double val) { return val < min ? min : (val > max ? max : val); }
 __device__ double Dot(const double3& lhs, const double3& rhs) { return lhs.x * rhs.x + lhs.y * rhs.y + lhs.z * rhs.z; }
 __device__ double Length(const double3& val) { return norm3d(val.x, val.y, val.z); }
 __device__ double Lerp(double y1, double y2, double x) { return y1 + (y2 - y1) * x; }
@@ -27,6 +28,16 @@ __device__ double3 Normalize(const double3 &val) { return val * rnorm3d(val.x, v
 
 // Matrix Math.
 struct Matrix4D { double M[16]; };
+
+__device__ Matrix4D CreateMatrixIdentity() {
+	double m[] = { 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 };
+	return *(Matrix4D*)m;
+}
+
+__device__ Matrix4D CreateMatrixTranslate(double x, double y, double z) {
+	double m[] = { 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, x, y, z, 1 };
+	return *(Matrix4D*)m;
+}
 
 __device__ double3 TransformPoint(const Matrix4D& m, const double3& p) {
 	return make_double3(
@@ -50,10 +61,6 @@ __device__ double4 Transform(const Matrix4D& m, const double4& p) {
 		m.M[3] * p.x + m.M[7] * p.y + m.M[11] * p.z + m.M[15] * p.w);
 }
 
-__device__ double Clamp(double min, double max, double val) {
-	return val < min ? min : (val > max ? max : val);
-}
-
 __device__ double3 Reflect(const double3& incident, const double3& normal) {
 	return incident - 2 * Dot(incident, normal) * normal;
 }
@@ -69,18 +76,10 @@ __device__ double3 Refract(const double3& incident, const double3& normal, doubl
 	return k < 0 ? make_double3(0, 0, 0) : (eta * incident + (eta * cosi - sqrt(k)) * n);
 }
 
-/*
-From GL Specification (https://www.opengl.org/registry/doc/GLSLangSpec.Full.1.20.8.pdf)
-k = 1.0 - eta * eta * (1.0 - dot(N, I) * dot(N, I))
-if (k < 0.0) return genType(0.0)
-else return eta * I - (eta * dot(N, I) + sqrt(k)) * N
-*/
-__device__ double3 RefractGL(const double3& incident, const double3 &normal, float ior)
-{
-	float N_dot_I = Dot(normal, incident);
-	float k = 1 - ior * ior * (1 - N_dot_I * N_dot_I);
-	if (k < 0) return make_double3(0, 0, 0);
-	else return ior * incident - (ior * N_dot_I + sqrtf(k)) * normal;
+__device__ double SchlickApprox(const double3& incident, const double3 &normal, double ior1, double ior2) {
+	double coeff = (ior1 - ior2) / (ior1 + ior2);
+	coeff = coeff * coeff;
+	return coeff + (1 - coeff) * pow((1 - Dot(-incident, normal)), 5);
 }
 
 // Geometric Math.
@@ -107,120 +106,96 @@ __device__ double IntersectSphere(const double3 &origin, const double3 &directio
 	return lambda_best;
 }
 
+enum GeometryType {
+	GEOMETRY_NONE = 0,
+	GEOMETRY_PLANE = 1,
+	GEOMETRY_SPHERE = 2
+};
+
+enum MaterialType {
+	MATERIAL_NONE = 0,
+	MATERIAL_CHECKERBOARD_XZ = 1,
+	MATERIAL_RED,
+	MATERIAL_GREEN,
+	MATERIAL_BLUE,
+	MATERIAL_GLASS
+};
+
+__device__ double Intersect(const double3 &origin, const double3 &direction, GeometryType geometry) {
+	switch (geometry) {
+	case GEOMETRY_PLANE:
+		return IntersectPlane(origin, direction);
+	case GEOMETRY_SPHERE:
+		return IntersectSphere(origin, direction);
+	default: return 1000000;
+	}
+}
+
+struct SceneObject {
+	Matrix4D Transform;
+	Matrix4D TransformInverse;
+	GeometryType Geometry;
+	MaterialType Material;
+};
+
+const int SCENE_COUNT = 4;
+
 // Raytracer Ray Tests.
 __device__ bool RayShadow(const double3 &origin, const double3 &direction) {
-	// Start intersecting objects.
-	double best_distance = 1000000;
-	double distance;
-	// Intersect with the ground plane
-	distance = IntersectPlane(origin, direction);
-	if (distance >= 0 && distance < best_distance) return true;
-	// Intersect with the red sphere.
-	{
-		double xfrm_inv[] = { 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 2, -1 ,0, 1 };
-		Matrix4D *matxfrm_inv = (Matrix4D*)xfrm_inv;
-		double3 origin2 = TransformPoint(*matxfrm_inv, origin);
-		double3 direction2 = TransformVector(*matxfrm_inv, direction);
-		distance = IntersectSphere(origin2, direction2);
-		if (distance >= 0 && distance < best_distance) return true;
-	}
-	// Intersect with the green sphere.
-	{
-		double xfrm_inv[] = { 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, -1 ,0, 1 };
-		Matrix4D *matxfrm_inv = (Matrix4D*)xfrm_inv;
-		double3 origin2 = TransformPoint(*matxfrm_inv, origin);
-		double3 direction2 = TransformVector(*matxfrm_inv, direction);
-		distance = IntersectSphere(origin2, direction2);
-		if (distance >= 0 && distance < best_distance) return true;
-	}
-	// Intersect with the blue sphere.
-	{
-		double xfrm_inv[] = { 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, -2, -1 ,0, 1 };
-		Matrix4D *matxfrm_inv = (Matrix4D*)xfrm_inv;
-		double3 origin2 = TransformPoint(*matxfrm_inv, origin);
-		double3 direction2 = TransformVector(*matxfrm_inv, direction);
-		distance = IntersectSphere(origin2, direction2);
-		if (distance >= 0 && distance < best_distance) return true;
+	const SceneObject TheScene[] = {
+		SceneObject{ CreateMatrixIdentity(), CreateMatrixIdentity(), GEOMETRY_PLANE, MATERIAL_CHECKERBOARD_XZ },
+		SceneObject{ CreateMatrixTranslate(-2, 1, 0), CreateMatrixTranslate(2, -1, 0), GEOMETRY_SPHERE, MATERIAL_RED },
+		SceneObject{ CreateMatrixTranslate(0, 1, 0), CreateMatrixTranslate(0, -1, 0), GEOMETRY_SPHERE, MATERIAL_GREEN },
+		SceneObject{ CreateMatrixTranslate(2, 1, 0), CreateMatrixTranslate(-2, -1, 0), GEOMETRY_SPHERE, MATERIAL_BLUE },
+	};
+	for (int i = 0; i < SCENE_COUNT; ++i) {
+		const SceneObject &scene_object = TheScene[i];
+		double3 transformed_origin = TransformPoint(scene_object.TransformInverse, origin);
+		double3 transformed_direction = TransformVector(scene_object.TransformInverse, direction);
+		double lambda = Intersect(transformed_origin, transformed_direction, scene_object.Geometry);
+		if (lambda >= 0 && lambda < 1000000) return true;
 	}
 	return false;
 }
 
-__device__ double4 RayColor(const double3 &origin, const double3 &direction, int recurse) {
-	double4 color = make_double4(0, 0, 0, 0);
+template <int RECURSE>
+__device__ double4 RayColor(const double3 &origin, const double3 &direction) {
 	// Start intersecting objects.
-	double best_distance = 1000000;
-	int best_object = 0;
-	double distance;
-	// Intersect with the ground plane
-	distance = IntersectPlane(origin, direction);
-	if (distance >= 0 && distance < best_distance) {
-		best_distance = distance;
-		best_object = 1;
-	}
-	// Intersect with the red sphere.
-	{
-		double xfrm_inv[] = { 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 2, -1 ,0, 1 };
-		Matrix4D *matxfrm_inv = (Matrix4D*)xfrm_inv;
-		double3 origin2 = TransformPoint(*matxfrm_inv, origin);
-		double3 direction2 = TransformVector(*matxfrm_inv, direction);
-		distance = IntersectSphere(origin2, direction2);
-		if (distance >= 0 && distance < best_distance) {
-			best_distance = distance;
-			best_object = 2;
+	const SceneObject TheScene[] = {
+		SceneObject{ CreateMatrixIdentity(), CreateMatrixIdentity(), GEOMETRY_PLANE, MATERIAL_CHECKERBOARD_XZ },
+		SceneObject{ CreateMatrixTranslate(-2, 1, 0), CreateMatrixTranslate(2, -1, 0), GEOMETRY_SPHERE, MATERIAL_RED },
+		SceneObject{ CreateMatrixTranslate(0, 1, 0), CreateMatrixTranslate(0, -1, 0), GEOMETRY_SPHERE, MATERIAL_GREEN },
+		SceneObject{ CreateMatrixTranslate(2, 1, 0), CreateMatrixTranslate(-2, -1, 0), GEOMETRY_SPHERE, MATERIAL_BLUE },
+	};
+	double best_lambda = 1000000;
+	int best_pobject = 0;
+	for (int i = 0; i < SCENE_COUNT; ++i) {
+		const SceneObject &scene_object = TheScene[i];
+		double3 transformed_origin = TransformPoint(scene_object.TransformInverse, origin);
+		double3 transformed_direction = TransformVector(scene_object.TransformInverse, direction);
+		double lambda = Intersect(transformed_origin, transformed_direction, scene_object.Geometry);
+		if (lambda >= 0 && lambda < best_lambda) {
+			best_lambda = lambda;
+			best_pobject = i;
 		}
 	}
-	// Intersect with the green sphere.
-	{
-		double xfrm_inv[] = { 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, -1 ,0, 1 };
-		Matrix4D *matxfrm_inv = (Matrix4D*)xfrm_inv;
-		double3 origin2 = TransformPoint(*matxfrm_inv, origin);
-		double3 direction2 = TransformVector(*matxfrm_inv, direction);
-		distance = IntersectSphere(origin2, direction2);
-		if (distance >= 0 && distance < best_distance) {
-			best_distance = distance;
-			best_object = 3;
-		}
+	if (best_lambda == 1000000) {
+		return make_double4(0, 0, 0, 0);
 	}
-	// Intersect with the blue sphere.
 	{
-		double xfrm_inv[] = { 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, -2, -1 ,0, 1 };
-		Matrix4D *matxfrm_inv = (Matrix4D*)xfrm_inv;
-		double3 origin2 = TransformPoint(*matxfrm_inv, origin);
-		double3 direction2 = TransformVector(*matxfrm_inv, direction);
-		distance = IntersectSphere(origin2, direction2);
-		if (distance >= 0 && distance < best_distance) {
-			best_distance = distance;
-			best_object = 4;
-		}
-	}
-	// Best was the ground plane.
-	if (best_object == 1) {
-		double3 p = origin + best_distance * direction;
+		double3 p = origin + best_lambda * direction;
 		double3 l = Normalize(make_double3(10, 10, -10) - p);
-		double3 n = make_double3(0, 1, 0);
-		double3 i = Normalize(direction);
-		double3 r = Reflect(i, n);
-		double scale_diffuse = Dot(l, n);
-		scale_diffuse = scale_diffuse > 0 ? scale_diffuse : 0;
-		double scale_specular = Dot(l, r);
-		scale_specular = scale_specular > 0 ? scale_specular : 0;
-		scale_specular = pow(scale_specular, 10);
-		if (RayShadow(p + 0.0001 * n, l)) scale_diffuse *= 0.5;
-		int mx = (p.x - floor(p.x)) < 0.5 ? 0 : 1;
-		int my = 0; // (space.Y - floor(space.Y)) < 0.5 ? 0 : 1;
-		int mz = (p.z - floor(p.z)) < 0.5 ? 0 : 1;
-		int mod = (mx + my + mz) % 2;
-		color.x = color.y = color.z = scale_diffuse * mod + scale_specular;
-		if (recurse > 0) {
-			color = color + RayColor(p + r * 0.0001, r, recurse - 1) * 0.5;
+		double3 n;
+		switch (TheScene[best_pobject].Geometry) {
+		case GEOMETRY_PLANE:
+			n = make_double3(0, 1, 0);
+			break;
+		case GEOMETRY_SPHERE:
+			n = TransformPoint(TheScene[best_pobject].TransformInverse, p);
+			break;
+		default:
+			n = make_double3(0, 1, 0);
 		}
-		color.w = 1;
-	}
-	// Best was the red sphere.
-	if (best_object == 2) {
-		double3 p = origin + best_distance * direction;
-		double3 l = Normalize(make_double3(10, 10, -10) - p);
-		double3 n = Normalize(p - make_double3(-2, 1, 0));
 		double3 i = Normalize(direction);
 		double3 r = Reflect(i, n);
 		double3 t = Refract(i, n, 1.5);
@@ -228,62 +203,82 @@ __device__ double4 RayColor(const double3 &origin, const double3 &direction, int
 		scale_diffuse = scale_diffuse > 0 ? scale_diffuse : 0;
 		double scale_specular = Dot(l, r);
 		scale_specular = scale_specular > 0 ? scale_specular : 0;
-		scale_specular = pow(scale_specular, 10);
-		double4 color_diffuse = make_double4(1, 0, 0, 0);
-		double4 color_specular = make_double4(1, 1, 1, 0);
-		//color = color_diffuse * scale_diffuse + color_specular * scale_specular;
-		color = color + color_specular * scale_specular;
-		if (recurse > 0) {
-			//color = color + RayColor(p + n * 0.0001, r, recurse - 1) * 0.5;
-			color = color + RayColor(p + t * 0.0001, t, recurse - 1);
+		scale_specular = pow(scale_specular, 100);
+		if (RayShadow(p + 0.0001 * n, l)) scale_diffuse *= 0.5;
+		double4 color_diffuse;
+		double4 color_specular;
+		double scale_reflect;
+		double scale_refract;
+		switch (TheScene[best_pobject].Material) {
+		case MATERIAL_CHECKERBOARD_XZ:
+		{
+			int mx = (p.x - floor(p.x)) < 0.5 ? 0 : 1;
+			int my = 0; // (space.Y - floor(space.Y)) < 0.5 ? 0 : 1;
+			int mz = (p.z - floor(p.z)) < 0.5 ? 0 : 1;
+			double c = (mx + my + mz) % 2;
+			color_diffuse = make_double4(c, c, c, 0);
+			color_specular = make_double4(1, 1, 1, 0);
+			scale_reflect = 1;
+			scale_refract = 0;
+		}
+		break;
+		case MATERIAL_RED:
+		{
+			//color_diffuse = make_double4(1, 0, 0, 0);
+			color_diffuse = make_double4(0, 0, 0, 0);
+			color_specular = make_double4(1, 1, 1, 0);
+			scale_reflect = 0;
+			scale_refract = 1;
+		}
+		break;
+		case MATERIAL_GREEN:
+		{
+			color_diffuse = make_double4(0, 0.5, 0, 0);
+			color_specular = make_double4(1, 1, 1, 0);
+			scale_reflect = 0.5;
+			scale_refract = 0;
+		}
+		break;
+		case MATERIAL_BLUE:
+		{
+			color_diffuse = make_double4(0, 0, 1, 0);
+			color_specular = make_double4(1, 1, 1, 0);
+			scale_reflect = 0.5;
+			scale_refract = 0;
+		}
+		break;
+		case MATERIAL_GLASS:
+		{
+			color_diffuse = make_double4(0, 0, 0, 0);
+			color_specular = make_double4(1, 1, 1, 0);
+			scale_reflect = 0;
+			scale_refract = 1;
+		}
+		break;
+		}
+		double4 color = color_diffuse * scale_diffuse + color_specular * scale_specular;
+		if (scale_refract > 0) {
+			//Schlick's Approximation for reflectance.
+			double R = SchlickApprox(i, n, 1, 1.5);
+			scale_reflect = R;
+			//Add refraction contribution.
+			color = color + RayColor<RECURSE - 1>(p + t * 0.0001, t) * scale_refract;
+		}
+		if (scale_reflect > 0) {
+			color = color + RayColor<RECURSE - 1>(p + r * 0.0001, r) * scale_reflect;
 		}
 		color.w = 1;
+		return color;
 	}
-	// Best was the green sphere.
-	if (best_object == 3) {
-		double3 p = origin + best_distance * direction;
-		double3 l = Normalize(make_double3(10, 10, -10) - p);
-		double3 n = Normalize(p - make_double3(0, 1, 0));
-		double3 i = Normalize(direction);
-		double3 r = Reflect(i, n);
-		double scale_diffuse = Dot(l, n);
-		scale_diffuse = scale_diffuse > 0 ? scale_diffuse : 0;
-		double scale_specular = Dot(l, r);
-		scale_specular = scale_specular > 0 ? scale_specular : 0;
-		scale_specular = pow(scale_specular, 10);
-		double4 color_diffuse = make_double4(0, 0.5, 0, 0);
-		double4 color_specular = make_double4(1, 1, 1, 0);
-		color = color_diffuse * scale_diffuse + color_specular * scale_specular;
-		if (recurse > 0) {
-			color = color + RayColor(p + r * 0.0001, r, recurse - 1) * 0.5;
-		}
-		color.w = 1;
-	}
-	// Best was the blue sphere.
-	if (best_object == 4) {
-		double3 p = origin + best_distance * direction;
-		double3 l = Normalize(make_double3(10, 10, -10) - p);
-		double3 n = Normalize(p - make_double3(2, 1, 0));
-		double3 i = Normalize(direction);
-		double3 r = Reflect(i, n);
-		double scale_diffuse = Dot(l, n);
-		scale_diffuse = scale_diffuse > 0 ? scale_diffuse : 0;
-		double scale_specular = Dot(l, r);
-		scale_specular = scale_specular > 0 ? scale_specular : 0;
-		scale_specular = pow(scale_specular, 10);
-		double4 color_diffuse = make_double4(0, 0, 1, 0);
-		double4 color_specular = make_double4(1, 1, 1, 0);
-		color = color_diffuse * scale_diffuse + color_specular * scale_specular;
-		if (recurse > 0) {
-			color = color + RayColor(p + r * 0.0001, r, recurse - 1) * 0.5;
-		}
-		color.w = 1;
-	}
-	return color;
+}
+
+template <>
+__device__ double4 RayColor<-1>(const double3 &origin, const double3 &direction) {
+	return make_double4(0, 0, 0, 0);
 }
 
 __device__ double4 RayColor(const double3 &origin, const double3 &direction) {
-	return RayColor(origin, direction, 4);
+	return RayColor<2>(origin, direction);
 }
 
 __device__ void ComputeRay(const Matrix4D &inverse_mvp, double clipx, double clipy, double3 &origin, double3 &direction) {
@@ -311,8 +306,8 @@ __global__ void cudaRaytraceKernel(Matrix4D inverse_mvp, void *bitmap_ptr, int b
 	double3 origin;
 	double3 direction;
 	double4 color = make_double4(0, 0, 0, 0);
-	const int X_SUPERSAMPLES = 2;
-	const int Y_SUPERSAMPLES = 2;
+	const int X_SUPERSAMPLES = 1;
+	const int Y_SUPERSAMPLES = 1;
 	for (int y_supersample = 1; y_supersample <= Y_SUPERSAMPLES; ++y_supersample) {
 		for (int x_supersample = 1; x_supersample <= X_SUPERSAMPLES; ++x_supersample) {
 			// Build a ray for this supersample.
