f73ee99d9a34bba0fa46e4e2c8181cf7f8c3d6e5
Adam Johnson <adamcplusplus.johnson@gmail.com>
2017-01-22 15:23:21 -0800

    Bit of cleanup.

--- RenderToyCPP/RenderToy.cpp
+++ RenderToyCPP/RenderToy.cpp
@@ -1,4 +1,11 @@
+////////////////////////////////////////////////////////////////////////////////
+// RenderToy - A bit of history that's now a bit of silicon...
+// Copyright (C) Adam Johnson 2017
+////////////////////////////////////////////////////////////////////////////////
+
 #include <memory>
+
+typedef void(RENDERFN)(void*, double*, void*, int, int, int);
 
 extern "C" void CPURaycast(void *pScene, double *pMVP, void *bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride);
 extern "C" void CPURaycastNormals(void *pScene, double *pMVP, void *bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride);
@@ -22,80 +29,37 @@ namespace RenderToy
 		}
 		static void RaycastCPU(array<unsigned char>^ scene, array<double>^ inverse_mvp, System::IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
 		{
-			// Take a copy of the scene (and unpin to free up the GC as quickly as possible).
-			int length = scene->Length;
-			std::unique_ptr<unsigned char[]> c_scene(new unsigned char[length]);
-			{
-				pin_ptr<unsigned char> pin_scene = &scene[0];
-				memcpy(c_scene.get(), pin_scene, length);
-			}
-			// Take a copy of the inverse MVP for the camera.
-			double c_inverse_mvp[16] = {
-				inverse_mvp[0], inverse_mvp[1], inverse_mvp[2], inverse_mvp[3],
-				inverse_mvp[4], inverse_mvp[5], inverse_mvp[6], inverse_mvp[7],
-				inverse_mvp[8], inverse_mvp[9], inverse_mvp[10], inverse_mvp[11],
-				inverse_mvp[12], inverse_mvp[13], inverse_mvp[14], inverse_mvp[15] };
-			// Render the scene (if possible).
-			CPURaycast(c_scene.get(), c_inverse_mvp, (void*)bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
+			MarshalAndCall(CPURaycast, scene, inverse_mvp, bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
 		}
 		static void RaycastNormalsCPU(array<unsigned char>^ scene, array<double>^ inverse_mvp, System::IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
 		{
-			// Take a copy of the scene (and unpin to free up the GC as quickly as possible).
-			int length = scene->Length;
-			std::unique_ptr<unsigned char[]> c_scene(new unsigned char[length]);
-			{
-				pin_ptr<unsigned char> pin_scene = &scene[0];
-				memcpy(c_scene.get(), pin_scene, length);
-			}
-			// Take a copy of the inverse MVP for the camera.
-			double c_inverse_mvp[16] = {
-				inverse_mvp[0], inverse_mvp[1], inverse_mvp[2], inverse_mvp[3],
-				inverse_mvp[4], inverse_mvp[5], inverse_mvp[6], inverse_mvp[7],
-				inverse_mvp[8], inverse_mvp[9], inverse_mvp[10], inverse_mvp[11],
-				inverse_mvp[12], inverse_mvp[13], inverse_mvp[14], inverse_mvp[15] };
-			// Render the scene (if possible).
-			CPURaycastNormals(c_scene.get(), c_inverse_mvp, (void*)bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
+			MarshalAndCall(CPURaycastNormals, scene, inverse_mvp, bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
 		}
 		static void RaytraceCPU(array<unsigned char>^ scene, array<double>^ inverse_mvp, System::IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
 		{
-			// Take a copy of the scene (and unpin to free up the GC as quickly as possible).
-			int length = scene->Length;
-			std::unique_ptr<unsigned char[]> c_scene(new unsigned char[length]);
-			{
-				pin_ptr<unsigned char> pin_scene = &scene[0];
-				memcpy(c_scene.get(), pin_scene, length);
-			}
-			// Take a copy of the inverse MVP for the camera.
-			double c_inverse_mvp[16] = {
-				inverse_mvp[0], inverse_mvp[1], inverse_mvp[2], inverse_mvp[3],
-				inverse_mvp[4], inverse_mvp[5], inverse_mvp[6], inverse_mvp[7],
-				inverse_mvp[8], inverse_mvp[9], inverse_mvp[10], inverse_mvp[11],
-				inverse_mvp[12], inverse_mvp[13], inverse_mvp[14], inverse_mvp[15] };
-			// Render the scene (if possible).
-			CPURaytrace(c_scene.get(), c_inverse_mvp, (void*)bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
+			MarshalAndCall(CPURaytrace, scene, inverse_mvp, bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
 		}
 		static void RaycastCUDA(array<unsigned char>^ scene, array<double>^ inverse_mvp, System::IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
 		{
-			// Take a copy of the scene (and unpin to free up the GC as quickly as possible).
-			int length = scene->Length;
-			std::unique_ptr<unsigned char[]> c_scene(new unsigned char[length]);
-			{
-				pin_ptr<unsigned char> pin_scene = &scene[0];
-				memcpy(c_scene.get(), pin_scene, length);
-			}
-			// Take a copy of the inverse MVP for the camera.
-			double c_inverse_mvp[16] = {
-				inverse_mvp[0], inverse_mvp[1], inverse_mvp[2], inverse_mvp[3],
-				inverse_mvp[4], inverse_mvp[5], inverse_mvp[6], inverse_mvp[7],
-				inverse_mvp[8], inverse_mvp[9], inverse_mvp[10], inverse_mvp[11],
-				inverse_mvp[12], inverse_mvp[13], inverse_mvp[14], inverse_mvp[15] };
-			// Render the scene (if possible).
-#ifdef CUDA_INSTALLED
-			CUDARaycast(c_scene.get(), c_inverse_mvp, (void*)bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
-#endif
+			#ifdef CUDA_INSTALLED
+			MarshalAndCall(CUDARaycast, scene, inverse_mvp, bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
+			#endif
 		}
 		static void RaycastNormalsCUDA(array<unsigned char>^ scene, array<double>^ inverse_mvp, System::IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
 		{
+			#ifdef CUDA_INSTALLED
+			MarshalAndCall(CUDARaycastNormals, scene, inverse_mvp, bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
+			#endif
+		}
+		static void RaytraceCUDA(array<unsigned char>^ scene, array<double>^ inverse_mvp, System::IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
+		{
+			#ifdef CUDA_INSTALLED
+			MarshalAndCall(CUDARaytrace, scene, inverse_mvp, bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
+			#endif
+		}
+	private:
+		static void MarshalAndCall(RENDERFN& fn, array<unsigned char>^ scene, array<double>^ inverse_mvp, System::IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
+		{
 			// Take a copy of the scene (and unpin to free up the GC as quickly as possible).
 			int length = scene->Length;
 			std::unique_ptr<unsigned char[]> c_scene(new unsigned char[length]);
@@ -109,30 +73,7 @@ namespace RenderToy
 				inverse_mvp[4], inverse_mvp[5], inverse_mvp[6], inverse_mvp[7],
 				inverse_mvp[8], inverse_mvp[9], inverse_mvp[10], inverse_mvp[11],
 				inverse_mvp[12], inverse_mvp[13], inverse_mvp[14], inverse_mvp[15] };
-			// Render the scene (if possible).
-#ifdef CUDA_INSTALLED
-			CUDARaycastNormals(c_scene.get(), c_inverse_mvp, (void*)bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
-#endif
-		}
-		static void RaytraceCUDA(array<unsigned char>^ scene, array<double>^ inverse_mvp, System::IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
-		{
-			// Take a copy of the scene (and unpin to free up the GC as quickly as possible).
-			int length = scene->Length;
-			std::unique_ptr<unsigned char[]> c_scene(new unsigned char[length]);
-			{
-				pin_ptr<unsigned char> pin_scene = &scene[0];
-				memcpy(c_scene.get(), pin_scene, length);
-			}
-			// Take a copy of the inverse MVP for the camera.
-			double c_inverse_mvp[16] = {
-				inverse_mvp[ 0], inverse_mvp[ 1], inverse_mvp[ 2], inverse_mvp[ 3],
-				inverse_mvp[ 4], inverse_mvp[ 5], inverse_mvp[ 6], inverse_mvp[ 7],
-				inverse_mvp[ 8], inverse_mvp[ 9], inverse_mvp[10], inverse_mvp[11],
-				inverse_mvp[12], inverse_mvp[13], inverse_mvp[14], inverse_mvp[15]};
-			// Render the scene (if possible).
-			#ifdef CUDA_INSTALLED
-			CUDARaytrace(c_scene.get(), c_inverse_mvp, (void*)bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
-			#endif
+			fn(c_scene.get(), c_inverse_mvp, (void*)bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
 		}
 	};
 }
