8a76175d67d5572992308c78f6a6cd1c88d7f0c5
Adam Johnson <adamcplusplus.johnson@gmail.com>
2017-01-31 22:58:30 -0800

    Add mouse translate to UWP.

--- RenderToyUWP/Controls/RenderViewport.xaml.cs
+++ RenderToyUWP/Controls/RenderViewport.xaml.cs
@@ -15,6 +15,7 @@ using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Runtime.InteropServices;
+using Windows.UI.Input;
 
 // The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -25,6 +26,35 @@ namespace RenderToy
         public RenderViewport()
         {
             this.InitializeComponent();
+            Repaint();
+        }
+        protected override void OnPointerMoved(PointerRoutedEventArgs e)
+        {
+            base.OnPointerMoved(e);
+            if (!isDragging) return;
+            PointerPoint dragTo = e.GetCurrentPoint(this);
+            double dx = dragTo.Position.X - dragFrom.Position.X;
+            double dy = dragTo.Position.Y - dragFrom.Position.Y;
+            x += dx * -0.05;
+            y += dy * 0.05;
+            dragFrom = dragTo;
+            Repaint();
+        }
+        protected override void OnPointerPressed(PointerRoutedEventArgs e)
+        {
+            base.OnPointerPressed(e);
+            CapturePointer(e.Pointer);
+            isDragging = true;
+            dragFrom = e.GetCurrentPoint(this);
+        }
+        protected override void OnPointerReleased(PointerRoutedEventArgs e)
+        {
+            base.OnPointerReleased(e);
+            isDragging = false;
+            ReleasePointerCapture(e.Pointer);
+        }
+        void Repaint()
+        {
             int RENDER_WIDTH = 512;
             int RENDER_HEIGHT = 512;
             var bitmap = new WriteableBitmap(RENDER_WIDTH, RENDER_HEIGHT);
@@ -34,7 +64,7 @@ namespace RenderToy
                 GCHandle handle = GCHandle.Alloc(image, GCHandleType.Pinned);
                 try
                 {
-                    RenderCS.Wireframe(Scene.Default, MathHelp.Invert(MathHelp.CreateMatrixTranslate(0, 2, -5)) * CameraPerspective.CreateProjection(0.01, 100.0, 45, 45), handle.AddrOfPinnedObject(), RENDER_WIDTH, RENDER_HEIGHT, 4 * RENDER_WIDTH);
+                    RenderCS.Wireframe(Scene.Default, MathHelp.Invert(MathHelp.CreateMatrixTranslate(x, y, z)) * CameraPerspective.CreateProjection(0.01, 100.0, 45, 45), handle.AddrOfPinnedObject(), RENDER_WIDTH, RENDER_HEIGHT, 4 * RENDER_WIDTH);
                 }
                 finally
                 {
@@ -48,5 +78,10 @@ namespace RenderToy
             bitmap.Invalidate();
             Component_Image.Source = bitmap;
         }
+        bool isDragging = false;
+        PointerPoint dragFrom;
+        double x = 0;
+        double y = 2;
+        double z = -5;
     }
 }
