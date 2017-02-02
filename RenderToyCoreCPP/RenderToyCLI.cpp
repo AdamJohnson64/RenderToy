1183761fd29899223363e4961330b10deec02854
Adam Johnson <adamcplusplus.johnson@gmail.com>
2017-02-01 20:18:08 -0800

    Share the interop code between WPF and UWP.

--- RenderToyCoreCPP/RenderToyCLI.cpp
+++ RenderToyCoreCPP/RenderToyCLI.cpp
@@ -22,88 +22,60 @@ namespace RenderToy
 		{
 			return ::HaveCUDA();
 		}
-		static void RaycastCPU(array<unsigned char>^ scene, array<double>^ inverse_mvp, System::IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
+		static void RaycastCPU(array<unsigned char>^ scene, array<unsigned char>^ inverse_mvp, System::IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
 		{
 			MarshalAndCall(CPURaycast, scene, inverse_mvp, bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
 		}
-		static void RaycastNormalsCPU(array<unsigned char>^ scene, array<double>^ inverse_mvp, System::IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
+		static void RaycastNormalsCPU(array<unsigned char>^ scene, array<unsigned char>^ inverse_mvp, System::IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
 		{
 			MarshalAndCall(CPURaycastNormals, scene, inverse_mvp, bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
 		}
-		static void RaycastTangentsCPU(array<unsigned char>^ scene, array<double>^ inverse_mvp, System::IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
+		static void RaycastTangentsCPU(array<unsigned char>^ scene, array<unsigned char>^ inverse_mvp, System::IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
 		{
 			MarshalAndCall(CPURaycastTangents, scene, inverse_mvp, bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
 		}
-		static void RaycastBitangentsCPU(array<unsigned char>^ scene, array<double>^ inverse_mvp, System::IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
+		static void RaycastBitangentsCPU(array<unsigned char>^ scene, array<unsigned char>^ inverse_mvp, System::IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
 		{
 			MarshalAndCall(CPURaycastBitangents, scene, inverse_mvp, bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
 		}
-		static void RaytraceCPUF32(array<unsigned char>^ scene, array<float>^ inverse_mvp, System::IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
+		static void RaytraceCPUF32(array<unsigned char>^ scene, array<unsigned char>^ inverse_mvp, System::IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
 		{
 			MarshalAndCall(CPUF32Raytrace, scene, inverse_mvp, bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
 		}
-		static void RaytraceCPUF64(array<unsigned char>^ scene, array<double>^ inverse_mvp, System::IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
+		static void RaytraceCPUF64(array<unsigned char>^ scene, array<unsigned char>^ inverse_mvp, System::IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
 		{
 			MarshalAndCall(CPUF64Raytrace, scene, inverse_mvp, bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
 		}
-		static void RaycastCUDA(array<unsigned char>^ scene, array<double>^ inverse_mvp, System::IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
+		static void RaycastCUDA(array<unsigned char>^ scene, array<unsigned char>^ inverse_mvp, System::IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
 		{
 			MarshalAndCall(CUDARaycast, scene, inverse_mvp, bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
 		}
-		static void RaycastNormalsCUDA(array<unsigned char>^ scene, array<double>^ inverse_mvp, System::IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
+		static void RaycastNormalsCUDA(array<unsigned char>^ scene, array<unsigned char>^ inverse_mvp, System::IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
 		{
 			MarshalAndCall(CUDARaycastNormals, scene, inverse_mvp, bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
 		}
-		static void RaycastTangentsCUDA(array<unsigned char>^ scene, array<double>^ inverse_mvp, System::IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
+		static void RaycastTangentsCUDA(array<unsigned char>^ scene, array<unsigned char>^ inverse_mvp, System::IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
 		{
 			MarshalAndCall(CUDARaycastTangents, scene, inverse_mvp, bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
 		}
-		static void RaycastBitangentsCUDA(array<unsigned char>^ scene, array<double>^ inverse_mvp, System::IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
+		static void RaycastBitangentsCUDA(array<unsigned char>^ scene, array<unsigned char>^ inverse_mvp, System::IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
 		{
 			MarshalAndCall(CUDARaycastBitangents, scene, inverse_mvp, bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
 		}
-		static void RaytraceCUDAF32(array<unsigned char>^ scene, array<float>^ inverse_mvp, System::IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
+		static void RaytraceCUDAF32(array<unsigned char>^ scene, array<unsigned char>^ inverse_mvp, System::IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
 		{
 			MarshalAndCall(CUDAF32Raytrace, scene, inverse_mvp, bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
 		}
-		static void RaytraceCUDAF64(array<unsigned char>^ scene, array<double>^ inverse_mvp, System::IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
+		static void RaytraceCUDAF64(array<unsigned char>^ scene, array<unsigned char>^ inverse_mvp, System::IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
 		{
 			MarshalAndCall(CUDAF64Raytrace, scene, inverse_mvp, bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
 		}
 	private:
-		static void MarshalAndCall(RENDERFN& fn, array<unsigned char>^ scene, array<float>^ inverse_mvp, System::IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
+		static void MarshalAndCall(RENDERFN& fn, array<unsigned char>^ scene, array<unsigned char>^ inverse_mvp, System::IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
 		{
-			// Take a copy of the scene (and unpin to free up the GC as quickly as possible).
-			int length = scene->Length;
-			std::unique_ptr<unsigned char[]> c_scene(new unsigned char[length]);
-			{
-				pin_ptr<unsigned char> pin_scene = &scene[0];
-				memcpy(c_scene.get(), pin_scene, length);
-			}
-			// Take a copy of the inverse MVP for the camera.
-			float c_inverse_mvp[16] = {
-				inverse_mvp[0], inverse_mvp[1], inverse_mvp[2], inverse_mvp[3],
-				inverse_mvp[4], inverse_mvp[5], inverse_mvp[6], inverse_mvp[7],
-				inverse_mvp[8], inverse_mvp[9], inverse_mvp[10], inverse_mvp[11],
-				inverse_mvp[12], inverse_mvp[13], inverse_mvp[14], inverse_mvp[15] };
-			fn(c_scene.get(), c_inverse_mvp, (void*)bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
-		}
-		static void MarshalAndCall(RENDERFN& fn, array<unsigned char>^ scene, array<double>^ inverse_mvp, System::IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
-		{
-			// Take a copy of the scene (and unpin to free up the GC as quickly as possible).
-			int length = scene->Length;
-			std::unique_ptr<unsigned char[]> c_scene(new unsigned char[length]);
-			{
-				pin_ptr<unsigned char> pin_scene = &scene[0];
-				memcpy(c_scene.get(), pin_scene, length);
-			}
-			// Take a copy of the inverse MVP for the camera.
-			double c_inverse_mvp[16] = {
-				inverse_mvp[0], inverse_mvp[1], inverse_mvp[2], inverse_mvp[3],
-				inverse_mvp[4], inverse_mvp[5], inverse_mvp[6], inverse_mvp[7],
-				inverse_mvp[8], inverse_mvp[9], inverse_mvp[10], inverse_mvp[11],
-				inverse_mvp[12], inverse_mvp[13], inverse_mvp[14], inverse_mvp[15] };
-			fn(c_scene.get(), c_inverse_mvp, (void*)bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
+			pin_ptr<unsigned char> pin_scene = &scene[0];
+			pin_ptr<unsigned char> pin_inverse_mvp = &inverse_mvp[0];
+			fn(pin_scene, pin_inverse_mvp, (void*)bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
 		}
 	};
 }
--- RenderToyWPF/Render/Render_CUDA.cs
+++ RenderToyWPF/Render/Render_CUDA.cs
@@ -19,69 +19,51 @@ namespace RenderToy
         }
         public static void RaycastCPU(Scene scene, Matrix3D mvp, IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
         {
-            RenderToy.RenderToyCPP.RaycastCPU(SceneFormatter.CreateFlatMemory(scene), MatrixToDoubles(MathHelp.Invert(mvp)), bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
+            RenderToy.RenderToyCPP.RaycastCPU(SceneFormatter.CreateFlatMemory(scene), SceneFormatter.CreateFlatMemory(MathHelp.Invert(mvp)), bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
         }
         public static void RaycastNormalsCPU(Scene scene, Matrix3D mvp, IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
         {
-            RenderToy.RenderToyCPP.RaycastNormalsCPU(SceneFormatter.CreateFlatMemory(scene), MatrixToDoubles(MathHelp.Invert(mvp)), bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
+            RenderToy.RenderToyCPP.RaycastNormalsCPU(SceneFormatter.CreateFlatMemory(scene), SceneFormatter.CreateFlatMemory(MathHelp.Invert(mvp)), bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
         }
         public static void RaycastTangentsCPU(Scene scene, Matrix3D mvp, IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
         {
-            RenderToy.RenderToyCPP.RaycastTangentsCPU(SceneFormatter.CreateFlatMemory(scene), MatrixToDoubles(MathHelp.Invert(mvp)), bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
+            RenderToy.RenderToyCPP.RaycastTangentsCPU(SceneFormatter.CreateFlatMemory(scene), SceneFormatter.CreateFlatMemory(MathHelp.Invert(mvp)), bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
         }
         public static void RaycastBitangentsCPU(Scene scene, Matrix3D mvp, IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
         {
-            RenderToy.RenderToyCPP.RaycastTangentsCPU(SceneFormatter.CreateFlatMemory(scene), MatrixToDoubles(MathHelp.Invert(mvp)), bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
+            RenderToy.RenderToyCPP.RaycastTangentsCPU(SceneFormatter.CreateFlatMemory(scene), SceneFormatter.CreateFlatMemory(MathHelp.Invert(mvp)), bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
         }
         public static void RaytraceCPUF32(Scene scene, Matrix3D mvp, IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
         {
-            RenderToy.RenderToyCPP.RaytraceCPUF32(SceneFormatter.CreateFlatMemoryF32(scene), MatrixToFloats(MathHelp.Invert(mvp)), bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
+            RenderToy.RenderToyCPP.RaytraceCPUF32(SceneFormatter.CreateFlatMemoryF32(scene), SceneFormatter.CreateFlatMemoryF32(MathHelp.Invert(mvp)), bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
         }
         public static void RaytraceCPUF64(Scene scene, Matrix3D mvp, IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
         {
-            RenderToy.RenderToyCPP.RaytraceCPUF64(SceneFormatter.CreateFlatMemory(scene), MatrixToDoubles(MathHelp.Invert(mvp)), bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
+            RenderToy.RenderToyCPP.RaytraceCPUF64(SceneFormatter.CreateFlatMemory(scene), SceneFormatter.CreateFlatMemory(MathHelp.Invert(mvp)), bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
         }
         public static void RaycastCUDA(Scene scene, Matrix3D mvp, IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
         {
-            RenderToy.RenderToyCPP.RaycastCUDA(SceneFormatter.CreateFlatMemory(scene), MatrixToDoubles(MathHelp.Invert(mvp)), bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
+            RenderToy.RenderToyCPP.RaycastCUDA(SceneFormatter.CreateFlatMemory(scene), SceneFormatter.CreateFlatMemory(MathHelp.Invert(mvp)), bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
         }
         public static void RaycastNormalsCUDA(Scene scene, Matrix3D mvp, IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
         {
-            RenderToy.RenderToyCPP.RaycastNormalsCUDA(SceneFormatter.CreateFlatMemory(scene), MatrixToDoubles(MathHelp.Invert(mvp)), bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
+            RenderToy.RenderToyCPP.RaycastNormalsCUDA(SceneFormatter.CreateFlatMemory(scene), SceneFormatter.CreateFlatMemory(MathHelp.Invert(mvp)), bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
         }
         public static void RaycastTangentsCUDA(Scene scene, Matrix3D mvp, IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
         {
-            RenderToy.RenderToyCPP.RaycastTangentsCUDA(SceneFormatter.CreateFlatMemory(scene), MatrixToDoubles(MathHelp.Invert(mvp)), bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
+            RenderToy.RenderToyCPP.RaycastTangentsCUDA(SceneFormatter.CreateFlatMemory(scene), SceneFormatter.CreateFlatMemory(MathHelp.Invert(mvp)), bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
         }
         public static void RaycastBitangentsCUDA(Scene scene, Matrix3D mvp, IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
         {
-            RenderToy.RenderToyCPP.RaycastBitangentsCUDA(SceneFormatter.CreateFlatMemory(scene), MatrixToDoubles(MathHelp.Invert(mvp)), bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
+            RenderToy.RenderToyCPP.RaycastBitangentsCUDA(SceneFormatter.CreateFlatMemory(scene), SceneFormatter.CreateFlatMemory(MathHelp.Invert(mvp)), bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
         }
         public static void RaytraceCUDAF32(Scene scene, Matrix3D mvp, IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
         {
-            RenderToy.RenderToyCPP.RaytraceCUDAF32(SceneFormatter.CreateFlatMemoryF32(scene), MatrixToFloats(MathHelp.Invert(mvp)), bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
+            RenderToy.RenderToyCPP.RaytraceCUDAF32(SceneFormatter.CreateFlatMemoryF32(scene), SceneFormatter.CreateFlatMemoryF32(MathHelp.Invert(mvp)), bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
         }
         public static void RaytraceCUDAF64(Scene scene, Matrix3D mvp, IntPtr bitmap_ptr, int bitmap_width, int bitmap_height, int bitmap_stride)
         {
-            RenderToy.RenderToyCPP.RaytraceCUDAF64(SceneFormatter.CreateFlatMemory(scene), MatrixToDoubles(MathHelp.Invert(mvp)), bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
-        }
-        static float[] MatrixToFloats(Matrix3D mvp)
-        {
-            return new float[16] {
-                (float)mvp.M11, (float)mvp.M12, (float)mvp.M13, (float)mvp.M14,
-                (float)mvp.M21, (float)mvp.M22, (float)mvp.M23, (float)mvp.M24,
-                (float)mvp.M31, (float)mvp.M32, (float)mvp.M33, (float)mvp.M34,
-                (float)mvp.M41, (float)mvp.M42, (float)mvp.M43, (float)mvp.M44,
-            };
-        }
-        static double[] MatrixToDoubles(Matrix3D mvp)
-        {
-            return new double[16] {
-                mvp.M11, mvp.M12, mvp.M13, mvp.M14,
-                mvp.M21, mvp.M22, mvp.M23, mvp.M24,
-                mvp.M31, mvp.M32, mvp.M33, mvp.M34,
-                mvp.M41, mvp.M42, mvp.M43, mvp.M44,
-            };
+            RenderToy.RenderToyCPP.RaytraceCUDAF64(SceneFormatter.CreateFlatMemory(scene), SceneFormatter.CreateFlatMemory(MathHelp.Invert(mvp)), bitmap_ptr, bitmap_width, bitmap_height, bitmap_stride);
         }
         #endregion
     }
