1fa70a91d8aadd6cc7eb50e4728ecff301a98a50
Adam Johnson <adamcplusplus.johnson@gmail.com>
2017-02-17 15:49:22 -0800

    Implement Matrix3D op==.

--- RenderToyCoreCS/Utility/MathHelp.cs
+++ RenderToyCoreCS/Utility/MathHelp.cs
@@ -224,6 +224,18 @@ namespace RenderToy
         public Point3D Transform(Point3D p) { return MathHelp.Transform(this, p); }
         public Point4D Transform(Point4D p) { return MathHelp.Transform(this, p); }
         public Vector3D Transform(Vector3D p) { return MathHelp.Transform(this, p); }
+        public static bool operator==(Matrix3D a, Matrix3D b)
+        {
+            return
+                a.M11 == b.M11 && a.M12 == b.M12 && a.M13 == b.M13 && a.M14 == b.M14 &&
+                a.M21 == b.M21 && a.M22 == b.M22 && a.M23 == b.M23 && a.M24 == b.M24 &&
+                a.M31 == b.M31 && a.M32 == b.M32 && a.M33 == b.M33 && a.M34 == b.M34 &&
+                a.M41 == b.M41 && a.M42 == b.M42 && a.M43 == b.M43 && a.M44 == b.M44;
+        }
+        public static bool operator!=(Matrix3D a, Matrix3D b)
+        {
+            return !(a == b);
+        }
     }
     public struct Quaternion
     {
