214324cf9bfa1c3d7259c85b7d33ce0a5820b3d4
Adam Johnson <adamcplusplus.johnson@gmail.com>
2017-02-26 22:05:08 -0800

    Use multipass in wire render.

--- RenderToyWPF/Controls/ViewWire.cs
+++ RenderToyWPF/Controls/ViewWire.cs
@@ -12,15 +12,28 @@ namespace RenderToy
 {
     class ViewWire : ViewBase
     {
+        public ViewWire()
+        {
+            render = new SinglePassAsyncAdaptor(RenderCall.Generate(typeof(RenderCS).GetMethod("WireframeCPUF64")), () => Dispatcher.Invoke(InvalidateVisual));
+        }
+        SinglePassAsyncAdaptor render;
         #region - Overrides : RenderViewportBase -
+        protected override void OnSceneChanged(Scene scene)
+        {
+            base.OnSceneChanged(scene);
+            if (render == null) return;
+            render.SetScene(scene);
+        }
         protected override void OnRenderToy(DrawingContext drawingContext)
         {
             int RENDER_WIDTH = (int)Math.Ceiling(ActualWidth);
             int RENDER_HEIGHT = (int)Math.Ceiling(ActualHeight);
+            render.SetCamera(MVP);
+            render.SetTarget(RENDER_WIDTH, RENDER_HEIGHT);
             if (RENDER_WIDTH == 0 || RENDER_HEIGHT == 0) return;
             WriteableBitmap bitmap = new WriteableBitmap(RENDER_WIDTH, RENDER_HEIGHT, 0, 0, PixelFormats.Bgra32, null);
             bitmap.Lock();
-            RenderCS.WireframeCPUF64(Scene, MVP, bitmap.BackBuffer, RENDER_WIDTH, RENDER_HEIGHT, bitmap.BackBufferStride);
+            render.CopyTo(bitmap.BackBuffer, RENDER_WIDTH, RENDER_HEIGHT, bitmap.BackBufferStride);
             bitmap.AddDirtyRect(new Int32Rect(0, 0, RENDER_WIDTH, RENDER_HEIGHT));
             bitmap.Unlock();
             drawingContext.DrawImage(bitmap, new Rect(0, 0, ActualWidth, ActualHeight));
