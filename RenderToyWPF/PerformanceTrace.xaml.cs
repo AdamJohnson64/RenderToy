75db79e992f4286701b601084fc7e688ccd515fd
Adam Johnson <adamcplusplus.johnson@gmail.com>
2017-03-11 17:03:56 -0800

    Add a perf time ruler.

--- RenderToyWPF/PerformanceTrace.xaml.cs
+++ RenderToyWPF/PerformanceTrace.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Data;
 using System.Windows.Input;
 using System.Windows.Media;
 using RenderToy.Linq;
+using System.Diagnostics;
 
 namespace RenderToy
 {
@@ -49,10 +50,23 @@ namespace RenderToy
             get { return (long)GetValue(TraceTimeMaxProperty); }
             set { SetValue(TraceTimeMaxProperty, value); }
         }
+        public static DependencyProperty TraceTimeBaseProperty = DependencyProperty.Register("TraceTimeBase", typeof(long), typeof(PerformanceTrackControl), new FrameworkPropertyMetadata((long)0, FrameworkPropertyMetadataOptions.AffectsRender));
+        public long TraceTimeBase
+        {
+            get { return (long)GetValue(TraceTimeBaseProperty); }
+            set { SetValue(TraceTimeBaseProperty, value); }
+        }
+        public static DependencyProperty TraceTimeFrequencyProperty = DependencyProperty.Register("TraceTimeFrequency", typeof(long), typeof(PerformanceTrackControl), new FrameworkPropertyMetadata((long)Stopwatch.Frequency, FrameworkPropertyMetadataOptions.AffectsRender));
+        public long TraceTimeFrequency
+        {
+            get { return (long)GetValue(TraceTimeFrequencyProperty); }
+            set { SetValue(TraceTimeFrequencyProperty, value); }
+        }
         #endregion
         #region - Section : Construction -
         public PerformanceTrackControl()
         {
+            ClipToBounds = true;
             // Normally we pass a trace text from a textbox to fill the control.
             // This converter will take the input and parse it into an enumerable set of events.
             SetBinding(TraceEventsProperty, new Binding
@@ -82,6 +96,13 @@ namespace RenderToy
                 RelativeSource = new RelativeSource(RelativeSourceMode.Self),
                 Path = new PropertyPath(TraceBinsProperty),
                 Converter = new Converter((bins) => ((IEnumerable<PerformanceTrack>)bins).SelectMany(track => track.Events).Select(ev => ev.End.Timestamp).MaxOrDefault())
+            });
+            // Set the timebase to the minimum time for clarity.
+            SetBinding(TraceTimeBaseProperty, new Binding
+            {
+                RelativeSource = new RelativeSource(RelativeSourceMode.Self),
+                Path = new PropertyPath(TraceBinsProperty),
+                Converter = new Converter((bins) => ((IEnumerable<PerformanceTrack>)bins).SelectMany(track => track.Events).Select(ev => ev.Begin.Timestamp).MinOrDefault())
             });
         }
         #endregion
@@ -154,33 +175,68 @@ namespace RenderToy
         protected override void OnRender(DrawingContext drawingContext)
         {
             base.OnRender(drawingContext);
+            // Create some stock rendering resources.
+            double RULER_OFFSET = 0;
+            double RULER_HEIGHT = 16;
+            double TRACE_OFFSET = RULER_HEIGHT;
+            double TRACE_HEIGHT = ActualHeight - RULER_HEIGHT;
+            Pen pen_black = new Pen(Brushes.Black, -1);
             // Draw a transparent background so hit-tests will always land in this control.
             drawingContext.DrawRectangle(Brushes.Transparent, null, new Rect(0, 0, ActualWidth, ActualHeight));
-            var tracks = TraceBins;
-            if (tracks == null) return;
             long timemin = TraceTimeMin;
             long timemax = TraceTimeMax;
-            // Render the performance data using the most attrociously bone-headed slow method known to humankind.
-            int trackcount = 0;
-            Pen pen_black = new Pen(Brushes.Black, -1);
-            foreach (var track in tracks)
+            ////////////////////////////////////////////////////////////////////////////////
+            // Draw the trace data.
+            var tracebins = TraceBins;
+            if (tracebins != null)
             {
-                foreach (var block in track.Events)
+                // Render the performance data using the most attrociously bone-headed slow method known to humankind.
+                int trackcount = 0;
+                foreach (var track in tracebins)
                 {
-                    double x1 = (block.Begin.Timestamp - timemin) * ActualWidth / (timemax - timemin);
-                    double x2 = (block.End.Timestamp - timemin) * ActualWidth / (timemax - timemin);
-                    if (x2 - x1 < 1) continue;
-                    if (x2 < 0 || x1 > ActualWidth) continue;
-                    double y1 = (trackcount + 0) * ActualHeight / tracks.Count;
-                    double y2 = (trackcount + 1) * ActualHeight / tracks.Count;
-                    var rect = new Rect(x1, y1, x2 - x1, y2 - y1);
-                    drawingContext.DrawRectangle(Brushes.LightGray, pen_black, rect);
-                    if (x2 - x1 < 32) continue;
-                    drawingContext.PushClip(new RectangleGeometry(new Rect(x1, y1, x2 - x1, y2 - y1)));
-                    drawingContext.DrawText(new FormattedText(block.Begin.Text, System.Globalization.CultureInfo.InvariantCulture, FlowDirection.LeftToRight, new Typeface("Arial"), 10, Brushes.Black), new Point(x1, y1));
-                    drawingContext.Pop();
+                    foreach (var block in track.Events)
+                    {
+                        double x1 = (block.Begin.Timestamp - timemin) * ActualWidth / (timemax - timemin);
+                        double x2 = (block.End.Timestamp - timemin) * ActualWidth / (timemax - timemin);
+                        if (x2 - x1 < 1) continue;
+                        if (x2 < 0 || x1 > ActualWidth) continue;
+                        double y1 = TRACE_OFFSET + (trackcount + 0) * TRACE_HEIGHT / tracebins.Count;
+                        double y2 = TRACE_OFFSET + (trackcount + 1) * TRACE_HEIGHT / tracebins.Count;
+                        var rect = new Rect(x1, y1, x2 - x1, y2 - y1);
+                        drawingContext.DrawRectangle(Brushes.LightGray, pen_black, rect);
+                        if (x2 - x1 < 32) continue;
+                        drawingContext.PushClip(new RectangleGeometry(new Rect(x1, y1, x2 - x1, y2 - y1)));
+                        drawingContext.DrawText(new FormattedText(block.Begin.Text, System.Globalization.CultureInfo.InvariantCulture, FlowDirection.LeftToRight, new Typeface("Arial"), 10, Brushes.Black), new Point(x1, y1));
+                        drawingContext.Pop();
+                    }
+                    ++trackcount;
                 }
-                ++trackcount;
+            }
+            ////////////////////////////////////////////////////////////////////////////////
+            // Draw a ruler.
+            drawingContext.DrawRectangle(Brushes.LightYellow, pen_black, new Rect(0, RULER_OFFSET, ActualWidth, RULER_HEIGHT));
+            // Get the ruler time at the left and right of the window.
+            double rulertimemin = (double)(timemin - TraceTimeBase) / TraceTimeFrequency;
+            double rulertimemax = (double)(timemax - TraceTimeBase) / TraceTimeFrequency;
+            // Determine an appropriate log10 timebase for a good number of ticks.
+            double rulertick_log10 = Math.Log10(rulertimemax - rulertimemin) - 1;
+            double rulertick_log10round = Math.Floor(rulertick_log10);
+            double rulertick_decimal = Math.Pow(10, rulertick_log10round);
+            // Round down rulermin and round up rulermax to the nearest division of timetick.
+            double rulerroundmin = Math.Floor(rulertimemin / rulertick_decimal) * rulertick_decimal;
+            double rulerroundmax = Math.Ceiling(rulertimemax / rulertick_decimal) * rulertick_decimal;
+            // Generate ticks in this range and plot them.
+            int maxticks = (int)Math.Ceiling((rulerroundmax - rulerroundmin) / rulertick_decimal);
+            for (int i = 0; i < maxticks; ++i)
+            {
+                // Calculate a time that should be in scale.
+                double rulertime = rulerroundmin + i * rulertick_decimal;
+                // Map this back to tick time.
+                double rulermap = (rulertime * TraceTimeFrequency) + TraceTimeBase;
+                // Map this tick time to screen space.
+                double rulerx = (rulermap - TraceTimeMin) / (TraceTimeMax - TraceTimeMin) * ActualWidth;
+                drawingContext.DrawLine(pen_black, new Point(rulerx, RULER_OFFSET + 2), new Point(rulerx, RULER_OFFSET + RULER_HEIGHT - 2));
+                drawingContext.DrawText(new FormattedText(rulertime.ToString(), CultureInfo.InvariantCulture, FlowDirection.LeftToRight, new Typeface("Arial"), 8, Brushes.Black), new Point(rulerx, RULER_OFFSET + 2));
             }
         }
         bool dragscrollenable = false;
