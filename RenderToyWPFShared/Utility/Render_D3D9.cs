1b31760851fad1c2147f9dd4069ac8a6a52696c4
Adam Johnson <adamcplusplus.johnson@gmail.com>
2018-05-14 10:25:01 -0700

    Use the pipeline to drive D3D9.

--- RenderToyWPFShared/Utility/Render_D3D9.cs
+++ RenderToyWPFShared/Utility/Render_D3D9.cs
@@ -8,7 +8,6 @@ using RenderToy.Primitives;
 using RenderToy.SceneGraph;
 using RenderToy.Utility;
 using System;
-using System.Linq;
 
 namespace RenderToy.WPF
 {
@@ -21,55 +20,26 @@ namespace RenderToy.WPF
             d3dsurface.BeginScene();
             foreach (var transformedobject in TransformedObject.Enumerate(scene))
             {
-                Matrix3D model_mvp = transformedobject.Transform * mvp;
                 IParametricUV uv = transformedobject.Node.Primitive as IParametricUV;
                 if (uv == null) continue;
                 d3dsurface.SetColor(Rasterization.ColorToUInt32(transformedobject.Node.WireColor));
-                Action<Vector4D, Vector4D, Vector4D> filltri_clipspace = (p1, p2, p3) =>
+                var A = PipelineModel.PrimitiveAssembly.CreateTriangles(uv);
+                var B = Transformation.Vector3ToVector4(A);
+                var C = Transformation.Transform(B, transformedobject.Transform * mvp);
+                var D = Clipping.ClipTriangle(C);
+                var E = Transformation.HomogeneousDivide(D);
+                var iter = E.GetEnumerator();
+                while (iter.MoveNext())
                 {
-                    var iter = Clipping.ClipTriangle(new Vector4D[] { p1, p2, p3 }).GetEnumerator();
-                    while (iter.MoveNext())
-                    {
-                        var P0 = iter.Current;
-                        if (!iter.MoveNext())
-                        {
-                            break;
-                        }
-                        var P1 = iter.Current;
-                        if (!iter.MoveNext())
-                        {
-                            break;
-                        }
-                        var P2 = iter.Current;
-                        Vector4D[] v3 = { P0, P1, P2 };
-                        Vector4D[] points = { P0, P1, P2 };
-                        var t = points
-                            .Select(p => new Vector4D(p.X / p.W, p.Y / p.W, p.Z / p.W, 1))
-                            .ToArray();
-                        d3dsurface.DrawTriangle(
-                            (float)t[0].X, (float)t[0].Y, (float)t[0].Z, (float)t[0].W,
-                            (float)t[1].X, (float)t[1].Y, (float)t[1].Z, (float)t[1].W,
-                            (float)t[2].X, (float)t[2].Y, (float)t[2].Z, (float)t[2].W);
-                    }
-                };
-                for (int v = 0; v < 10; ++v)
-                {
-                    for (int u = 0; u < 10; ++u)
-                    {
-                        Vector3D[] points =
-                        {
-                            uv.GetPointUV((u + 0.0) / 10, (v + 0.0) / 10),
-                            uv.GetPointUV((u + 1.0) / 10, (v + 0.0) / 10),
-                            uv.GetPointUV((u + 0.0) / 10, (v + 1.0) / 10),
-                            uv.GetPointUV((u + 1.0) / 10, (v + 1.0) / 10),
-                        };
-                        var t = points
-                            .Select(x => new Vector4D(x.X, x.Y, x.Z, 1))
-                            .Select(x => model_mvp.Transform(x))
-                            .ToArray();
-                        filltri_clipspace(t[0], t[1], t[3]);
-                        filltri_clipspace(t[3], t[2], t[0]);
-                    }
+                    var P0 = iter.Current;
+                    if (!iter.MoveNext()) break;
+                    var P1 = iter.Current;
+                    if (!iter.MoveNext()) break;
+                    var P2 = iter.Current;
+                    d3dsurface.DrawTriangle(
+                        (float)P0.X, (float)P0.Y, (float)P0.Z, (float)P0.W,
+                        (float)P1.X, (float)P1.Y, (float)P1.Z, (float)P1.W,
+                        (float)P2.X, (float)P2.Y, (float)P2.Z, (float)P2.W);
                 }
             }
             d3dsurface.EndScene();
