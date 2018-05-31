eaaf060b37fd1a29251f02ff03c04e061d980f82
Adam Johnson <adamcplusplus.johnson@gmail.com>
2018-05-30 17:34:02 -0700

    Apply headers and footers to the document.
    We use templates to define document fragments and then abuse the paginator to recreate the document in multi-page form with repeated headers and footers.

--- RenderToyWPF/MainWindow.xaml.cs
+++ RenderToyWPF/MainWindow.xaml.cs
@@ -21,11 +21,85 @@ using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Media;
 using System.Windows.Xps.Packaging;
 using System.Windows.Xps.Serialization;
 
 namespace RenderToy.WPF
 {
+    public class DocumentPaginatorWrapper : DocumentPaginator
+    {
+        public static DependencyProperty PageNumberProperty = DependencyProperty.RegisterAttached("PageNumber", typeof(int), typeof(DocumentPaginatorWrapper), new FrameworkPropertyMetadata(0, FrameworkPropertyMetadataOptions.AffectsArrange | FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsRender | FrameworkPropertyMetadataOptions.Inherits));
+        public DocumentPaginatorWrapper(DocumentPaginator paginator, double header, double footer, DataTemplate templateHeader, DataTemplate templateFooter)
+        {
+            heightHeader = header;
+            heightFooter = footer;
+            innerPaginator = paginator;
+            this.templateHeader = templateHeader;
+            this.templateFooter = templateFooter;
+        }
+        public override DocumentPage GetPage(int pageNumber)
+        {
+            DocumentPage page = null;
+            page = (this == innerPaginator) ? new DocumentPage(new ContainerVisual()) : innerPaginator.GetPage(pageNumber);
+            var newpage = new ContainerVisual();
+            newpage.SetValue(PageNumberProperty, pageNumber);
+            {
+                var sectiondocument = (FlowDocument)templateHeader.LoadContent();
+                sectiondocument.ColumnWidth = PageSize.Width;
+                var sectionpaginator = ((IDocumentPaginatorSource)sectiondocument).DocumentPaginator;
+                sectionpaginator.PageSize = PageSize;
+                var sectionpage = sectionpaginator.GetPage(0);
+                var sectioncontainer = new ContainerVisual();
+                sectioncontainer.Offset = new Vector(0, 0);
+                sectioncontainer.Children.Add(sectionpage.Visual);
+                newpage.Children.Add(sectioncontainer);
+            }
+            {
+                var sectioncontainer = new ContainerVisual();
+                sectioncontainer.Children.Add(page.Visual);
+                sectioncontainer.Offset = new Vector(0, heightHeader);
+                newpage.Children.Add(sectioncontainer);
+            }
+            {
+                var sectiondocument = (FlowDocument)templateFooter.LoadContent();
+                sectiondocument.SetValue(PageNumberProperty, pageNumber);
+                sectiondocument.ColumnWidth = PageSize.Width;
+                var sectionpaginator = ((IDocumentPaginatorSource)sectiondocument).DocumentPaginator;
+                sectionpaginator.PageSize = PageSize;
+                var sectionpage = sectionpaginator.GetPage(0);
+                var sectioncontainer = new ContainerVisual();
+                sectioncontainer.Offset = new Vector(0, heightHeader + page.Size.Height);
+                sectioncontainer.Children.Add(sectionpage.Visual);
+                newpage.Children.Add(sectioncontainer);
+            }
+            return new DocumentPage(newpage, PageSize, page.BleedBox, page.ContentBox);
+        }
+        public override bool IsPageCountValid { get { return innerPaginator.IsPageCountValid; } }
+        public override int PageCount { get { return innerPaginator.PageCount; } }
+        public override Size PageSize
+        {
+            get
+            {
+                var size = innerPaginator.PageSize;
+                size.Height = size.Height + heightHeader + heightFooter;
+                return size;
+            }
+            set
+            {
+                if (this != innerPaginator)
+                {
+                    innerPaginator.PageSize = value;
+                }
+            }
+        }
+        public override IDocumentPaginatorSource Source { get { return innerPaginator.Source; } }
+        DataTemplate templateHeader;
+        DataTemplate templateFooter;
+        double heightHeader;
+        double heightFooter;
+        DocumentPaginator innerPaginator;
+    }
     public partial class MainWindow : Window
     {
         public static RoutedUICommand CommandSceneNew = new RoutedUICommand("New Scene", "CommandSceneNew", typeof(View3DUser));
@@ -121,15 +195,16 @@ namespace RenderToy.WPF
                     {
                         using (var package = Package.Open(stream, FileMode.Create, FileAccess.ReadWrite))
                         {
-                            using (var xpsDoc = new XpsDocument(package, CompressionOption.Maximum))
+                            using (var xpsdocument = new XpsDocument(package, CompressionOption.Maximum))
                             {
                                 var flowdocument = (FlowDocument)Application.Current.Resources["RenderToyDocument"];
-                                flowdocument.ColumnWidth = 512;
-                                var rsm = new XpsSerializationManager(new XpsPackagingPolicy(xpsDoc), false);
-                                var paginator = ((IDocumentPaginatorSource)flowdocument).DocumentPaginator;
+                                var headerTemplate = (DataTemplate)flowdocument.Resources["HeaderTemplate"];
+                                var footerTemplate = (DataTemplate)flowdocument.Resources["FooterTemplate"];
+                                var serialization = new XpsSerializationManager(new XpsPackagingPolicy(xpsdocument), false);
+                                var paginator = new DocumentPaginatorWrapper(((IDocumentPaginatorSource)flowdocument).DocumentPaginator, 100, 100, headerTemplate, footerTemplate);
                                 paginator.PageSize = new System.Windows.Size(1024, 1280);
-                                rsm.SaveAsXaml(paginator);
-                                rsm.Commit();
+                                serialization.SaveAsXaml(paginator);
+                                serialization.Commit();
                             }
                         }
                     }
