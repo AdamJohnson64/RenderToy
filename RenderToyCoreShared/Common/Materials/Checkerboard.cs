d9e54e8d73730a699773a5ad80f6e0f27cbfeb0a
Adam Johnson <adamcplusplus.johnson@gmail.com>
2018-06-27 16:08:45 -0700

    Improve the checkerboard HLSL.

--- RenderToyCoreShared/Common/Materials/Checkerboard.cs
+++ RenderToyCoreShared/Common/Materials/Checkerboard.cs
@@ -3,6 +3,7 @@
 // Copyright (C) Adam Johnson 2018
 ////////////////////////////////////////////////////////////////////////////////
 
+using RenderToy.Expressions;
 using RenderToy.Math;
 using RenderToy.Utility;
 using System;
@@ -13,26 +14,32 @@ namespace RenderToy.Materials
     sealed class Checkerboard : MNSample2D<Vector4D>, IMNNode<Vector4D>, INamed
     {
         public string Name { get { return "Checkerboard"; } }
+        static Expression<Func<double, double>> FloorFn2 = (d) => System.Math.Floor(d);
+        static Expression<Func<double, double>> FloorFn = ExpressionReducer.Rename(FloorFn2, "Floor");
+        static Expression<Func<double, double>> CheckerboardFn2()
+        {
+            var parameter = Expression.Parameter(typeof(double), "d");
+            return (Expression<Func<double, double>>)Expression.Lambda(Expression.Multiply(Expression.Subtract(parameter, Expression.Invoke(FloorFn, parameter)), Expression.Constant(2.0)), "ContinuousSawtoothWave", new[] { parameter });
+        }
+        static Expression<Func<double, double>> CheckerboardFn = CheckerboardFn2();
+        static Expression<Func<double, int>> SquareFn2()
+        {
+            var parameter = Expression.Parameter(typeof(double), "d");
+            return (Expression<Func<double, int>>)Expression.Lambda(Expression.Convert(Expression.Invoke(CheckerboardFn, parameter), typeof(int)), "ContinuousSquareWave", new[] { parameter });
+        }
+        static Expression<Func<double, int>> SquareFn = SquareFn2();
         public Expression CreateExpression(Expression evalcontext)
         {
-            var tempu = Expression.Parameter(typeof(double), "u");
-            var tempv = Expression.Parameter(typeof(double), "v");
-            var intu = Expression.Parameter(typeof(int), "nu");
-            var intv = Expression.Parameter(typeof(int), "nv");
-            return Expression.Block(
-                typeof(Vector4D),
-                new ParameterExpression[] { tempu, tempv, intu, intv },
-                new Expression[]
-                {
-                    Expression.Assign(tempu, u.CreateExpression(evalcontext)),
-                    Expression.Assign(tempv, v.CreateExpression(evalcontext)),
-                    Expression.Assign(intu, Expression.Convert(Expression.Multiply(Expression.Subtract(tempu, Expression.Call(null, typeof(System.Math).GetMethod("Floor", new Type[] { typeof(double) }), new Expression[] { tempu })), Expression.Constant(2.0)), typeof(int))),
-                    Expression.Assign(intv, Expression.Convert(Expression.Multiply(Expression.Subtract(tempv, Expression.Call(null, typeof(System.Math).GetMethod("Floor", new Type[] { typeof(double) }), new Expression[] { tempv })), Expression.Constant(2.0)), typeof(int))),
-                    Expression.Condition(
-                        Expression.Equal(Expression.And(Expression.Add(intu, intv), Expression.Constant(1)), Expression.Constant(0)),
-                        color1.CreateExpression(evalcontext),
-                        color2.CreateExpression(evalcontext)),
-                });
+            var paramu = Expression.Parameter(typeof(int), "u");
+            var paramv = Expression.Parameter(typeof(int), "v");
+            var lambda = Expression.Lambda(
+                Expression.Condition(
+                    Expression.Equal(Expression.And(Expression.Add(paramu, paramv), Expression.Constant(1)), Expression.Constant(0)),
+                    color1.CreateExpression(evalcontext),
+                    color2.CreateExpression(evalcontext)), "Checkerboard", new[] { paramu, paramv });
+            return Expression.Invoke(lambda,
+                Expression.Invoke(SquareFn, u.CreateExpression(evalcontext)),
+                Expression.Invoke(SquareFn, v.CreateExpression(evalcontext)));
         }
         public IMNNode<Vector4D> Color1 { get { return color1; } set { color1 = value; } }
         public IMNNode<Vector4D> Color2 { get { return color2; } set { color2 = value; } }
