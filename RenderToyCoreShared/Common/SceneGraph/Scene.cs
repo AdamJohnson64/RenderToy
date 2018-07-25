a3af0c5458921ec6b6ccda4f5a6e533a27b5bb55
Adam Johnson <adamcplusplus.johnson@gmail.com>
2018-07-24 22:56:22 -0700

    Restore the default scene.

--- RenderToyCoreShared/Common/SceneGraph/Scene.cs
+++ RenderToyCoreShared/Common/SceneGraph/Scene.cs
@@ -26,32 +26,47 @@ namespace RenderToy.SceneGraph
     }
     public static class TestScenes
     {
-        public static IScene DefaultScene
-        {
-            get { return scene; }
-        }
+        public static IScene DefaultScene { get; private set; }
+        public static IScene DefaultScene2 { get; private set; }
         static TestScenes()
         {
-            scene = new Scene();
-            var materialBrick = new LoaderOBJ.OBJMaterial();
             {
-                materialBrick.map_Kd = StockMaterials.Brick;
-                var displace = new MNSubtract
-                {
-                    Lhs = new BrickMask { U = new MNMultiply { Lhs = new MNTexCoordU(), Rhs = new MNConstant { Value = 4 } }, V = new MNMultiply { Lhs = new MNTexCoordV(), Rhs = new MNConstant { Value = 4 } } },
-                    Rhs = new MNMultiply { Lhs = new Perlin2D { U = new MNMultiply { Lhs = new MNTexCoordU(), Rhs = new MNConstant { Value = 512 } }, V = new MNMultiply { Lhs = new MNTexCoordV(), Rhs = new MNConstant { Value = 512 } } }, Rhs = new MNConstant { Value = 0.001 } }
-                };
-                materialBrick.map_bump = new BumpGenerate { U = new MNTexCoordU(), V = new MNTexCoordV(), Displacement = displace };
-                materialBrick.displacement = new MNVector4D { R = displace, G = displace, B = displace, A = new MNConstant { Value = 1 } };
+                Scene scene = new Scene();
+                scene.children.Add(new Node("Plane Ground", new TransformMatrix(MathHelp.CreateMatrixScale(10, 10, 10)), Plane.Default, StockMaterials.LightGray, StockMaterials.MarbleTile));
+                scene.children.Add(new Node("Sphere (Red)", new TransformMatrix(MathHelp.CreateMatrixTranslate(-5, 1, 0)), Sphere.Default, StockMaterials.Red, StockMaterials.PlasticRed));
+                scene.children.Add(new Node("Sphere (Green)", new TransformMatrix(MathHelp.CreateMatrixTranslate(-3, 1, 0)), Mesh.CreateMesh(Sphere.Default, 18, 9), StockMaterials.Green, StockMaterials.PlasticGreen));
+                scene.children.Add(new Node("Sphere (Blue)", new TransformMatrix(MathHelp.CreateMatrixTranslate(-1, 1, 0)), Sphere.Default, StockMaterials.Blue, StockMaterials.PlasticBlue));
+                scene.children.Add(new Node("Sphere (Yellow)", new TransformMatrix(MathHelp.CreateMatrixTranslate(+1, 1, 0)), Sphere.Default, StockMaterials.Yellow, StockMaterials.PlasticYellow));
+                scene.children.Add(new Node("Cube (Magenta)", new TransformMatrix(MathHelp.CreateMatrixTranslate(+3, 1, 0)), Cube.Default, StockMaterials.Magenta, StockMaterials.Brick));
+                scene.children.Add(new Node("Sphere (Cyan)", new TransformMatrix(MathHelp.CreateMatrixTranslate(+5, 1, 0)), Sphere.Default, StockMaterials.Cyan, StockMaterials.PlasticCyan));
+                scene.children.Add(new Node("Sphere (Glass)", new TransformMatrix(MathHelp.CreateMatrixTranslate(0, 3, 0)), Sphere.Default, StockMaterials.Black, StockMaterials.Glass));
+                AddOpenVR(scene);
+                DefaultScene = scene;
             }
-            scene.children.Add(new Node("Plane Ground", new TransformMatrix(MathHelp.CreateMatrixScale(10, 10, 10)), Plane.Default, StockMaterials.LightGray, materialBrick));
-            scene.children.Add(new Node("Sphere (Red)", new TransformMatrix(MathHelp.CreateMatrixTranslate(-5, 1, 0)), Sphere.Default, StockMaterials.Red, StockMaterials.PlasticRed));
-            scene.children.Add(new Node("Sphere (Green)", new TransformMatrix(MathHelp.CreateMatrixTranslate(-3, 1, 0)), Mesh.CreateMesh(Sphere.Default, 18, 9), StockMaterials.Green, StockMaterials.PlasticGreen));
-            scene.children.Add(new Node("Sphere (Blue)", new TransformMatrix(MathHelp.CreateMatrixTranslate(-1, 1, 0)), Sphere.Default, StockMaterials.Blue, StockMaterials.PlasticBlue));
-            scene.children.Add(new Node("Sphere (Yellow)", new TransformMatrix(MathHelp.CreateMatrixTranslate(+1, 1, 0)), Sphere.Default, StockMaterials.Yellow, StockMaterials.PlasticYellow));
-            scene.children.Add(new Node("Cube (Magenta)", new TransformMatrix(MathHelp.CreateMatrixTranslate(+3, 1, 0)), Cube.Default, StockMaterials.Magenta, StockMaterials.Brick));
-            scene.children.Add(new Node("Sphere (Cyan)", new TransformMatrix(MathHelp.CreateMatrixTranslate(+5, 1, 0)), Sphere.Default, StockMaterials.Cyan, StockMaterials.PlasticCyan));
-            scene.children.Add(new Node("Sphere (Glass)", new TransformMatrix(MathHelp.CreateMatrixTranslate(0, 3, 0)), Sphere.Default, StockMaterials.Black, StockMaterials.Glass));
+            {
+                Scene scene = new Scene();
+                var materialBrick = new LoaderOBJ.OBJMaterial();
+                {
+                    materialBrick.map_Kd = StockMaterials.Brick;
+                    var displace = new MNSubtract
+                    {
+                        Lhs = new BrickMask { U = new MNMultiply { Lhs = new MNTexCoordU(), Rhs = new MNConstant { Value = 4 } }, V = new MNMultiply { Lhs = new MNTexCoordV(), Rhs = new MNConstant { Value = 4 } } },
+                        Rhs = new MNMultiply { Lhs = new Perlin2D { U = new MNMultiply { Lhs = new MNTexCoordU(), Rhs = new MNConstant { Value = 512 } }, V = new MNMultiply { Lhs = new MNTexCoordV(), Rhs = new MNConstant { Value = 512 } } }, Rhs = new MNConstant { Value = 0.001 } }
+                    };
+                    materialBrick.map_bump = new BumpGenerate { U = new MNTexCoordU(), V = new MNTexCoordV(), Displacement = displace };
+                    materialBrick.displacement = new MNVector4D { R = displace, G = displace, B = displace, A = new MNConstant { Value = 1 } };
+                }
+                scene.children.Add(new Node("Plane Ground", new TransformMatrix(MathHelp.CreateMatrixScale(10, 10, 10)), Plane.Default, StockMaterials.LightGray, materialBrick));
+                scene.children.Add(new Node("Back Left", new TransformMatrix(MathHelp.CreateMatrixTranslate(-2, 1, -2)), Sphere.Default, StockMaterials.Red, materialBrick));
+                scene.children.Add(new Node("Back Right", new TransformMatrix(MathHelp.CreateMatrixTranslate(2, 1, -2)), Sphere.Default, StockMaterials.Green, materialBrick));
+                scene.children.Add(new Node("Front Left", new TransformMatrix(MathHelp.CreateMatrixTranslate(-2, 1, 2)), Sphere.Default, StockMaterials.Blue, materialBrick));
+                scene.children.Add(new Node("Front Right", new TransformMatrix(MathHelp.CreateMatrixTranslate(2, 1, 2)), Sphere.Default, StockMaterials.White, materialBrick));
+                AddOpenVR(scene);
+                DefaultScene2 = scene;
+            }
+        }
+        static void AddOpenVR(Scene scene)
+        {
             try
             {
                 var openvr = new OpenVRHelper();
@@ -72,6 +87,5 @@ namespace RenderToy.SceneGraph
                 Debug.WriteLine("WARNING: Unable to load controller model.");
             }
         }
-        static Scene scene;
     }
 }
