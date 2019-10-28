6b3c997c98cacac936c58cd05da323d47e3a0059
Adam Johnson <adamcplusplus.johnson@gmail.com>
2019-10-28 11:47:35 -0700

    Get rid of silly file.

--- Source/DrawingViewD3D9.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Arcturus.Managed
-{
-    static class Direct3D9
-    {
-        public static IDevice3D_D3D9 Device = new IDevice3D_D3D9();
-    }
-}
--- Source/DrawingViewD3DImage.cs
+++ Source/DrawingViewD3DImage.cs
@@ -7,6 +7,10 @@ using System.Windows.Threading;
 
 namespace Arcturus.Managed
 {
+    static class Direct3D9
+    {
+        public static IDevice3D_D3D9 Device = new IDevice3D_D3D9();
+    }
     abstract class DrawingViewD3DImage : ContentControl
     {
         static DrawingViewD3DImage()
