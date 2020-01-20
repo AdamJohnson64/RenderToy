ddaf6200f2321de8d2967e57449ba4d5b85b7e84
Adam Johnson <aj@nowhere.com>
2020-01-19 21:43:41 -0600

    Now try it in Linux.

---
+++ build.sh
@@ -0,0 +1,1 @@
+clang++ -IRenderToyCLI -Wno-everything -std=c++11 main_dc_text.cpp RenderToyCLI/Arcturus/DrawingContextCPU.cpp RenderToyCLI/Arcturus/DrawingContextReference.cpp RenderToyCLI/Arcturus/Vector.cpp
