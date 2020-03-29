835f0b3a8dedd0404013c4a28e5a08a69df83d9d
Adam Johnson <aj@nowhere.com>
2020-03-28 18:38:10 -0700

    We can sort out Linux later...

--- build.sh
+++
@@ -1,1 +0,0 @@
-clang++ -IRenderToyCLI -Wno-everything -std=c++11 main_dc_text.cpp RenderToyCLI/Arcturus/DrawingContextCPU.cpp RenderToyCLI/Arcturus/DrawingContextReference.cpp RenderToyCLI/Arcturus/Vector.cpp
